1f72e2bf064979045a5554fa8157a9ef8b747238
Brian Farrell <farrelb6@tcd.ie>
2024-10-04 11:48:28 -0700

    Disabled KillPlayerPatch for now

--- LethalCans/Patches/KillPlayerPatch.cs
+++ LethalCans/Patches/KillPlayerPatch.cs
@@ -1,28 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using HarmonyLib;
-using GameNetcodeStuff;
-using UnityEngine;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using HarmonyLib;
+// using GameNetcodeStuff;
+// using UnityEngine;
 
-namespace LethalCans.Patches
-{
-    [HarmonyPatch(typeof(PlayerControllerB))]
-    [HarmonyPatch("KillPlayer")]
-    public class KillPlayerPatch
-    {
-        // Postfix to run after KillPlayer method is executed
-        static void Postfix(PlayerControllerB __instance, Vector3 bodyVelocity, bool spawnBody, CauseOfDeath causeOfDeath, int deathAnimation, Vector3 positionOffset)
-        {
-            if (spawnBody)
-            {
-                // Set position of dead body
-                Vector3 deathPosition = __instance.transform.position;
-                int deadPlayerClientId = __instance.playerClientId;
-                // Check for observers of body
-                int spectators = DrinkTracker.calculateSpectators(deathPosition, deadPlayerClientId);
-            }
-        }
-    }
-}
+// namespace LethalCans.Patches
+// {
+//     [HarmonyPatch(typeof(PlayerControllerB))]
+//     [HarmonyPatch("KillPlayer")]
+//     public class KillPlayerPatch
+//     {
+//         // Postfix to run after KillPlayer method is executed
+//         static void Postfix(PlayerControllerB __instance, Vector3 bodyVelocity, bool spawnBody, CauseOfDeath causeOfDeath, int deathAnimation, Vector3 positionOffset)
+//         {
+//             if (spawnBody)
+//             {
+//                 // Set position of dead body
+//                 Vector3 deathPosition = __instance.transform.position;
+//                 int deadPlayerClientId = __instance.playerClientId;
+//                 // Check for observers of body
+//                 int spectators = DrinkTracker.calculateSpectators(deathPosition, deadPlayerClientId);
+//             }
+//         }
+//     }
+// }
