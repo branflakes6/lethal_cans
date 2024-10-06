4ed98e8263dd493323fb6893400c54ca685c5b40
Brian Farrell <farrelb6@tcd.ie>
2024-10-06 15:35:16 -0700

    Updated getDrinks

--- LethalCans/DrinkTracker.cs
+++ LethalCans/DrinkTracker.cs
@@ -20,7 +20,7 @@ namespace LethalCans
             Vector3 deathPosition = player.transform.position;
 
             int spectators = calculateSpectators(deathPosition, playerId);
-
+            Plugin.Instance.PluginLogger.LogDebug($"Specators: {spectators}");
             // This gets the death tag from coroner
             var deathTag = ((Coroner.AdvancedCauseOfDeath)causeOfDeath).GetLanguageTag();
             int medium = 1;
@@ -31,6 +31,7 @@ namespace LethalCans
             switch (deathTag)
             {
                 // Generic deaths
+                #region GenericDeaths
                 case "DeathBludgeoning":
                 case "DeathGravity":
                 case "DeathBlast":
@@ -42,60 +43,52 @@ namespace LethalCans
                 case "DeathDrowning":
                 case "DeathAbandoned":
                 case "DeathElectrocution":
-                case "DeathKicking": // New in v45
-                case "DeathBurning": // New in v50
-                case "DeathStabbing": // New in v50
-                case "DeathFan": // New in v50
-                case "DeathInertia": // New in v55
-                case "DeathSnipped": // New in v55:
-                    return 3 * difficulty;
+                case "DeathKicking":       // New in v45
+                case "DeathBurning":       // New in v50
+                case "DeathStabbing":      // New in v50
+                case "DeathFan":           // New in v50
+                case "DeathInertia":       // New in v55
+                case "DeathSnipped":       // New in v55
+                    return (3 * difficulty) + spectators;
+                #endregion
 
                 // Custom causes (enemies)
+                #region CustomCausesEnemies
                 case "DeathEnemyBaboonHawk":
-                    return 10 * difficulty;
+                    return (10 * difficulty) + spectators;
                 case "DeathEnemyBracken":
-                    return 2 * difficulty;
                 case "DeathEnemyBunkerSpider":
-                    return 2 * difficulty;
                 case "DeathEnemyCircuitBees":
                 case "DeathEnemyCoilHead":
-                    return 2 * difficulty;
                 case "DeathEnemyEarthLeviathan":
-                    return 2 * difficulty;
-                case "DeathEnemyEyelessDog":
-                    return 6 * difficulty;
-                case "DeathEnemyGhostGirl":
-                    return 6 * difficulty;
-                case "DeathEnemyHoarderBug":
-                    return 10 * difficulty;
-                case "DeathEnemyHygrodere":
-                    return 10 * difficulty;
-                case "DeathEnemyJester":
-                    return 6 * difficulty;
-                case "DeathEnemyLassoMan":
-                    return 10 * difficulty;
                 case "DeathEnemySnareFlea":
-                    return 2 * difficulty;
-                case "DeathEnemySporeLizard":
-                    return 10 * difficulty;
+                    return (2 * difficulty) + spectators;
+                case "DeathEnemyEyelessDog":
+                case "DeathEnemyGhostGirl":
+                case "DeathEnemyJester":
                 case "DeathEnemyThumper":
-                    return 6 * difficulty;
                 case "DeathEnemyForestGiantEaten":
-                    return 6 * difficulty;
+                    return (6 * difficulty) + spectators;
+                case "DeathEnemyHoarderBug":
+                case "DeathEnemyHygrodere":
+                case "DeathEnemyLassoMan":
+                case "DeathEnemySporeLizard":
                 case "DeathEnemyForestGiantDeath":
-                    return 10 * difficulty;
+                    return (10 * difficulty) + spectators;
+                #endregion
 
                 // Enemies from v45
+                #region EnemiesV45
                 case "DeathEnemyMaskedPlayerWear":
-                    return 10 * difficulty;
+                    return (10 * difficulty) + spectators;
                 case "DeathEnemyMaskedPlayerVictim":
-                    return 6 * difficulty;
                 case "DeathEnemyNutcrackerKicked":
-                    return 6 * difficulty;
                 case "DeathEnemyNutcrackerShot":
-                    return 6 * difficulty;
+                    return (6 * difficulty) + spectators;
+                #endregion
 
                 // Enemies from v50
+                #region EnemiesV50
                 case "DeathEnemyButlerStab":
                 case "DeathEnemyButlerExplode":
                 case "DeathEnemyMaskHornets":
@@ -104,59 +97,60 @@ namespace LethalCans
                 case "DeathEnemyOldBirdStomp":
                 case "DeathEnemyOldBirdCharge":
                 case "DeathEnemyOldBirdTorch":
-                    return 4 * difficulty;
+                    return (4 * difficulty) + spectators;
+                #endregion
 
                 // Enemies from v55
+                #region EnemiesV55
                 case "DeathEnemyKidnapperFox":
-                    return 4 * difficulty;
                 case "DeathEnemyBarber":
-                    return 4 * difficulty;
+                    return (4 * difficulty) + spectators;
+                #endregion
 
                 // Enemies from v60
+                #region EnemiesV60
                 case "DeathEnemyManeater":
-                    return 4 * difficulty;
+                    return (4 * difficulty) + spectators;
+                #endregion
 
                 // Custom causes (player)
+                #region CustomCausesPlayer
                 case "DeathPlayerJetpackGravity":
-                    return 4 * difficulty;
                 case "DeathPlayerJetpackBlast":
-                    return 4 * difficulty;
                 case "DeathPlayerLadder":
-                    return 4 * difficulty;
                 case "DeathPlayerMurderShovel":
-                    return 4 * difficulty;
                 case "DeathPlayerMurderStopSign":
-                    return 4 * difficulty;
                 case "DeathPlayerMurderYieldSign":
-                    return 4 * difficulty;
                 case "DeathPlayerMurderKnife":
-                    return 4 * difficulty;
                 case "DeathPlayerEasterEgg":
-                    return 4 * difficulty;
                 case "DeathPlayerMurderShotgun":
-                    return 4 * difficulty;
                 case "DeathPlayerQuicksand":
-                    return 4 * difficulty;
                 case "DeathPlayerStunGrenade":
-                    return 4 * difficulty;
+                    return (4 * difficulty) + spectators;
+                #endregion
 
                 // Custom causes (player vehicles)
+                #region CustomCausesPlayerVehicles
                 case "DeathPlayerCruiserDriver":
                 case "DeathPlayerCruiserPassenger":
                 case "DeathPlayerCruiserExplodeBystander":
                 case "DeathPlayerCruiserRanOver":
-                    return 4 * difficulty;
+                    return (4 * difficulty) + spectators;
+                #endregion 
 
                 // Custom causes (pits)
+                #region CustomCausesPits
                 case "DeathPitGeneric":
                 case "DeathPitFacilityPit":
                 case "DeathPitFacilityCatwalkJump":
                 case "DeathPitMinePit":
                 case "DeathPitMineCave":
                 case "DeathPitMineElevator":
-                    return 4 * difficulty;
+                    return (4 * difficulty) + spectators;
+                #endregion
 
                 // Custom causes (other)
+                #region CustomCausesOther
                 case "DeathOtherDepositItemsDesk":
                 case "DeathOtherItemDropship":
                 case "DeathOtherLandmine":
@@ -166,15 +160,16 @@ namespace LethalCans
                 case "DeathOtherSpikeTrap":
                 case "DeathOtherOutOfBounds":
                     Debug.Log("Player died due to dropship.");
-                    return 10 * difficulty;
+                    return (10 * difficulty) + spectators;
+                #endregion
 
                 // Unknown death cause
                 case "DeathUnknown":
-                    return 4 * difficulty;
+                    return (4 * difficulty) + spectators;
 
                 // Default case if no match
                 default:
-                    return 2;
+                    return 2 + spectators;
             }
         }
 
